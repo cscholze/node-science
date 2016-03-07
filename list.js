96114297cd56eb2e984f99444151ed3ac243deec
cScholze <caleb.scholze@gmail.com>
2016-03-07 14:40:32 -0600

    added tail pointer to reduce computation power

--- list.js
+++ list.js
@@ -1,36 +1,34 @@
-'use strict'
+'use strict';
 
 class Node {
   constructor (data) {
-    this.payload = data
-    this.next = null
+    this.payload = data;
+    this.next = null;
   }
 }
 
 class List {
   constructor () {
-    this.head = null
+    this.head = null;
+    this.tail = null;
   }
 
   add (node) {
     if (this.head) {
-      let current = this.head
-
-      while(current.next) {
-        current = current.next
-      }
-      current.next = node
+      this.tail.next = node;
+      this.tail = node;
     } else {
-      this.head = node
+      this.head = node;
+      this.tail = node;
     }
   }
 }
 
-const list = new List
-list.add(new Node('A'))
-list.add(new Node('B'))
-list.add(new Node('C'))
-list.add(new Node('D'))
-list.add(new Node('E'))
-list.add(new Node('F'))
-console.log(list)
+const list = new List;
+list.add(new Node('A'));
+list.add(new Node('B'));
+list.add(new Node('C'));
+list.add(new Node('D'));
+list.add(new Node('E'));
+list.add(new Node('F'));
+console.log(list);
