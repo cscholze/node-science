65aecba7efb28c5f19bdf09b2e3238880914f0c4
cScholze <caleb.scholze@gmail.com>
2016-03-09 10:25:25 -0600

    updated stack.js

--- stack.js
+++ stack.js
@@ -1,44 +1,83 @@
-'use strict';
+'use strict'
 
 class Node {
-  constructor(data) {
-    this.payload = data;
-    this.next = null;
+  constructor (data) {
+    this.payload = data
+    this.next = null
   }
 }
 
 class Stack {
-  constructor() {
-    this.top = null;
-  }
+  constructor (size) {
+    this.head = null
+    this.tail = null
+    this.length = 0
 
-  push(item) {
-    let node;
-
-    if (item instanceof Node ) {
-      node = item;
-    }
-    else {
-      node = new Node(item);
+    for (let i = 0; i < size; i++) {
+      this.push(new Node())
     }
 
-    node.next = this.top;
-    this.top = node;
+    this.maxSize = size
+    this.length = 0
   }
 
-  pop() {
-    const removed = this.top;
-    this.top = removed.next;
-    return removed;
+  push (item) {
+    if (this.isFull()) {
+      throw new Error('Stack full')
+    }
+
+    let node
+
+    if (item instanceof Node) {
+      node = item
+    } else {
+      node = new Node(item)
+    }
+
+    if (this.maxSize) {
+      let current = this.head
+
+      while(current.next !== this.tail) {
+        current = current.next
+      }
+
+      current.next = null
+    } else if (!this.tail) {
+      this.tail = node
+    }
+
+    node.next = this.head
+    this.head = node
+
+    this.length++
+  }
+
+  pop () {
+    if (!length) {
+      throw new Error('Stack empty')
+    }
+
+    const toRemove = this.head
+    const emptyNode = new Node()
+
+    this.head = toRemove.next
+    this.tail.next = emptyNode
+    this.tail = emptyNode
+    this.length--
+
+    return toRemove
+  }
+
+  isEmpty () {
+    return !this.length
+  }
+
+  isFull () {
+    return this.length >= this.maxSize
+  }
+
+  getSize () {
+    return this.length
   }
 }
-
-const stack = new Stack();
-stack.push(new Node('A'));
-stack.push(new Node('B'));
-stack.push(new Node('C'));
-stack.push('D');
-
-console.log(stack.pop());
-
 
