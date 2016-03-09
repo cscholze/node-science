9114ef9e1d6ddcee9bc26e0aafb39d1657041ea7
cScholze <caleb.scholze@gmail.com>
2016-03-09 10:25:12 -0600

    queue.js updated

--- queue.js
+++ queue.js
@@ -1,42 +1,34 @@
-'use strict';
+'use strict'
 
 class Node {
-  constructor(data) {
-    this.payload = data;
-    this.next = null;
+  constructor (data) {
+    this.payload = data
+    this.next = null
   }
 }
 
 class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
+  constructor () {
+    this.head = null
+    this.tail = null
   }
 
-  enqueue(node) {
+  enqueue (node) {
     if (this.head) {
-      this.tail.next = node;
-    }
-    else {
-      this.head = node;
+      this.tail.next = node
+    } else {
+      this.head = node
     }
 
-    this.tail = node;
+    this.tail = node
   }
 
   dequeue () {
-    const removed = this.head;
-    this.head = removed.next;
-    return removed;
+    this.head = this.head.next
+
+    if (!this.head) {
+      this.tail = null
+    }
   }
 }
-
-const queue = new Queue();
-queue.enqueue(new Node('A'))
-queue.enqueue(new Node('B'));
-queue.enqueue(new Node('C'));
-queue.enqueue(new Node('D'));
-
-console.log(queue.dequeue());
-
 
